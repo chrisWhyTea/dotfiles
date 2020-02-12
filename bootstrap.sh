b0cd9a2820b4be54521332ea8243b1f0c106823c
Christopher Schmitt <52947016+chrisWhyTea@users.noreply.github.com>
2020-02-13 00:00:51 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,9 @@
+# This stuff should be executed regardles of the env its deployed to. 
+# To make it more easy to understand whats currently happening, 
+# each command should have a echo that provides a description
+
 echo "Symlink ~/dotfiles/.gitconfig to ~/.gitconfig"
 ln -s -f $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+
+echo "Symlink ~/dotfiles/.zshrc to ~/.zshrc"
+ln -s -f $HOME/dotfiles/.zshrc $HOME/.zshrc
