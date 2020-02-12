9dc3352f651d202064900646df7906da5cd7c0b5
Christopher Schmitt <52947016+chrisWhyTea@users.noreply.github.com>
2020-02-12 23:32:31 +0100

    Update bootstrap-python.sh

--- bootstrap-python.sh
+++ bootstrap-python.sh
@@ -1,8 +1,7 @@
 sh bootstrap.sh
 
-echo "Install poetry and pipenv"
+echo "Install pipenv"
 pip3 install --user pipenv
-pip3 install --user poetry
 
 echo "Install python extension for vscode"
 code --install-extension ms-python.python
