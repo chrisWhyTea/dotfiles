1723dcbbd551c843064a86e9f74c6124ced01796
Christopher Schmitt <52947016+chrisWhyTea@users.noreply.github.com>
2020-02-12 21:58:50 +0100

    Update vsonline-python-setup.sh

--- vsonline-python-setup.sh
+++ vsonline-python-setup.sh
@@ -1,6 +1,4 @@
-# Generate ssh-key for this vsonline session
-ssh-keygen -t ed25519 -f "/home/vsonline/.ssh/id_vsonline" -P ""
-
+./vsonline-setup.sh
 # Install Pipenv and Poetry
 pip3 install --user poetry pipenv
 
