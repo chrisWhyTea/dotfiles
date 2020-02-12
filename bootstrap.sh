fd8a924aabbfff892de99258b73507d0a0583dae
Christopher Schmitt <52947016+chrisWhyTea@users.noreply.github.com>
2020-02-13 00:10:55 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 # To make it more easy to understand whats currently happening, 
 # each command should have a echo that provides a description
 
+echo "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 echo "Symlink ~/dotfiles/.gitconfig to ~/.gitconfig"
 ln -s -f $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 
