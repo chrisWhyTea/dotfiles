2bac711f19653e8413f3831e80b87b16c48bb56f
Christopher Schmitt <52947016+chrisWhyTea@users.noreply.github.com>
2020-02-20 17:56:13 +0100

    Create bootstrap-crostini.sh

---
+++ bootstrap-crostini.sh
@@ -0,0 +1,4 @@
+apt-key adv --refresh-keys --keyserver keyserver.ubuntu.com
+apt update
+apt upgrade -y
+apt install zsh nano make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl -y
